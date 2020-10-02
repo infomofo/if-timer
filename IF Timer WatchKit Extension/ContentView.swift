ceedb769b01343708b97e4c613b1d4605eed5b93
Will Chiong <infomofo@users.noreply.github.com>
2020-10-01 23:28:17 -0400

    Set up basic scaffolding for IF Timer App (#1)
    * set basic timer
    
    * add timer
    
    * add timer
    
    * add README, basic styling

--- IF Timer WatchKit Extension/ContentView.swift
+++ IF Timer WatchKit Extension/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .padding()
+    @State var countdownToDate: Date?
+    @State var interval: TimeInterval?
+    @State var hours: Int = 14
+    
+    let hourRange = 12...16
+    
+    let formatter = DateComponentsFormatter()
+    init () {
+        formatter.unitsStyle = .positional
+        formatter.includesApproximationPhrase = false
+        formatter.includesTimeRemainingPhrase = false
+        formatter.allowedUnits = [.hour,.minute,.second]
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        if let unwrappedDate = countdownToDate {
+            VStack {
+                Text(formatter.string(from: interval ?? 0) ?? "starting")
+                    .onReceive(timer) { input in
+                        self.interval = unwrappedDate.timeIntervalSince(Date())
+                    }
+                Button(action: {
+                    self.countdownToDate = nil
+                    self.interval = nil
+                }) {
+                    Text("Stop")
+                }
+            }
+        } else {
+            VStack {
+                // TODO: This text will be replaced by some sort of input element, maybe a picker?
+                // The user will be able to pick a number of hours
+                Text("Begin timer for \(self.hours) hours")
+                // Clicking start will begin a countdown timer for a number of hours
+                Button(action: {
+                    let hoursFromNow: TimeInterval = 60.0*60.0*Double(self.hours)
+                    self.countdownToDate = Date(timeInterval: hoursFromNow, since: Date())
+                }) {
+                    Text("Start")
+                }
+            }
+        }
+        
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,17 @@
+# IF Timer
+
+This app will be a basic hour countdown timer.
+
+It will have two states:
+- If a timer is not currently set, the user will be prompted to enter a number of hours, and hit start
+- If a timer is set, the user will be shown the number of hours, minutes, and seconds until the timer goes off, they
+  will also be given a button that lets them stop the current timer which returns it to the other state.
+
+## TODO
+
+Remaining todo
+
+- Make number of hours configurable- this should be a step function that the user can enter on the watch or the phone
+- Make the phone and watch sync on major events
+- Send a notification when the timer ends
+- Allow the watch to resume state and go to sleep when you're not using it, without losing time
